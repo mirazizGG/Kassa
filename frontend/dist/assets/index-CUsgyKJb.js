b6cbc1cecea855f6be7bf52e7a08598f51d980c2
Miraziz <mirazizmiragzamov8434@gmail.com>
2026-09-03 16:16:05 +0500

    miraziz bosh admin: admin hisoblarini faqat u boshqaradi

--- frontend/dist/assets/index-CUsgyKJb.js
+++
@@ -1,1 +0,0 @@
-import{r as o}from"./index-DFVR8V1B.js";function i(t,[r,e]){return Math.min(e,Math.max(r,t))}var n=o.createContext(void 0);function c(t){const r=o.useContext(n);return t||r||"ltr"}export{i as c,c as u};
