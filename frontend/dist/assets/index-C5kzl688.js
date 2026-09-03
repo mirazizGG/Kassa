2cd26ecdd48ef39bd8607153b6b00a257babf1b8
Miraziz <mirazizmiragzamov8434@gmail.com>
2026-09-03 16:30:05 +0500

    shtrix-kod bo'yicha internetdan mahsulot nomini topish

---
+++ frontend/dist/assets/index-C5kzl688.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index-GrQybHGw.js";function i(t,[r,e]){return Math.min(e,Math.max(r,t))}var n=o.createContext(void 0);function c(t){const r=o.useContext(n);return t||r||"ltr"}export{i as c,c as u};
---
+++ frontend/dist/assets/skeleton-CiPjUpkm.js
@@ -0,0 +1,1 @@
+import{j as s,n as t}from"./index-GrQybHGw.js";function m({className:e,...n}){return s.jsx("div",{className:t("animate-pulse rounded-md bg-muted",e),...n})}export{m as S};
